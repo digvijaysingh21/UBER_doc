140251ca1ca50321c9b09b047760615e85b4c197
digvijaysingh21 <digvijay2042000@gmail.com>
2025-03-07 22:17:46 +0530

    added  security or validator in UserProtectedWrapper

--- frontend/src/pages/UserProtectedWrapper.jsx
+++ frontend/src/pages/UserProtectedWrapper.jsx
@@ -1,11 +1,14 @@
-import React , {useContext, useEffect}from 'react'
+import React , {useContext, useEffect, useState}from 'react'
 import { UserDataContext } from '../context/userContext'
 import { useNavigate } from 'react-router-dom'
+import { locals } from '../../../Backend/app';
 
 const UserProtectedWrapper = ({children}) => {
   // const {user} = useContext(UserDataContext);
   const token = localStorage.getItem('token');
   const navigate = useNavigate();
+  const {user, setUser} = useContext(UserDataContext)
+  const [isLoading, setLoading] = useState(true);
 
   // console.log(token);
 
@@ -18,7 +21,29 @@ const UserProtectedWrapper = ({children}) => {
     if(!token){
       navigate('/login')
     }
+
+    axios.get(`${import.meta.env.VITE_BASE_URL}/users/profile`,{
+      headers:{
+        Authorization: `Bearer ${token}`
+      }
+    }).then(response =>{
+      if(response.status === 200){
+        setUser(response.data.user)
+        setLoading(false);
+      }
+    }).catch(err=>{
+      console.log(err);
+      localStorage.removeItem('token')
+      navigate('/login')
+    })
   },[token])
+
+  if(isLoading){
+    return (
+      <div>Loadng...</div>
+
+    )
+  }
   
   return (
     <>
